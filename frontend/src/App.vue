f03ead2509121d847a2359ed35e14350fc7fe3a5
s1809585 <s.saxena-1@sms.ed.ac.uk>
2022-04-21 14:04:52 +0100

    Added Support for Cointelegraph Articles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <ArticlesComponent />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ArticlesComponent from "./components/ArticlesComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ArticlesComponent,
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +20,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  list-style: none;
 }
 </style>
---
+++ frontend/src/components/ArticlesComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="article_container">
+        <div class="article_content">
+            <h1>Articles</h1>
+            <ul class="article_list">
+                <li v-for="article in articles" :key="article.uid">
+                    <h2>{{ article.title }}</h2>
+                    <p>{{ article.description }}</p>
+                    <button @click="readArticle(article)"> Read Article</button>
+                    <button @click="likeArticle(article)">+1</button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    console.log("Hello")
+
+    export default {
+        data() {
+            return {
+                // articles
+                articles: ['']
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    // fetch articles
+                    this.axios.get('http://127.0.0.1:8000/news/newsapi/').then(response => (this.articles = response.data)); 
+                } catch (error) {
+                    // log the error
+                    console.log(error);
+                }
+            },
+        },
+        created() {
+            // Fetch tasks on page load
+            this.getData();
+        }
+    }
+</script>
